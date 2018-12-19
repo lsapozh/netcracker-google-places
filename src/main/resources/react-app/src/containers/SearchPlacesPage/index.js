bf3163550574122e2075ffd290e71174570d640b
Liliia Sapozhnikova <sapozhnikova111@gmail.com>
2018-12-19 13:44:46 +0700

    some fixes

--- src/main/resources/react-app/src/containers/SearchPlacesPage/index.js
+++ src/main/resources/react-app/src/containers/SearchPlacesPage/index.js
@@ -1,6 +1,163 @@
 import React, { Component } from 'react'
 import Page from '../../components/Page'
 import SearchPlacesPageView from '../../components/SearchPlacesPageView'
+const dataTest = [
+    {
+        name: 'Place 1',
+        address: 'Address 1',
+        distance: {
+            humanReadable: '1 km',
+            inMeters: 1000,
+        },
+        duration: {
+            humanReadable: '10 min',
+            inSeconds: 600,
+        },
+        lat: 53.2,
+        lng: 49.5,
+    },
+    {
+        name: 'Place 2',
+        address: 'Address 2',
+        distance: {
+            humanReadable: '2 km',
+            inMeters: 2000,
+        },
+        duration: {
+            humanReadable: '11 min',
+            inSeconds: 660,
+        },
+        lat: 53.2,
+        lng: 49.6,
+    },
+    {
+        name: 'Place 3',
+        address: 'Address 3',
+        distance: {
+            humanReadable: '1.5 km',
+            inMeters: 1500,
+        },
+        duration: {
+            humanReadable: '7 min',
+            inSeconds: 420,
+        },
+        lat: 53.2,
+        lng: 49.7,
+    },
+    {
+        name: 'Place 4',
+        address: 'Address 4',
+        distance: {
+            humanReadable: '1.8 km',
+            inMeters: 1800,
+        },
+        duration: {
+            humanReadable: '13 min',
+            inSeconds: 780,
+        },
+        lat: 53.2,
+        lng: 49.8,
+    },
+    {
+        name: 'Place 5',
+        address: 'Address 5',
+        distance: {
+            humanReadable: '3 km',
+            inMeters: 3000,
+        },
+        duration: {
+            humanReadable: '20 min',
+            inSeconds: 1200,
+        },
+        lat: 53.2,
+        lng: 49.9,
+    },
+    {
+        name: 'Place 5',
+        address: 'Address 5a',
+        distance: {
+            humanReadable: '6 km',
+            inMeters: 6000,
+        },
+        duration: {
+            humanReadable: '40 min',
+            inSeconds: 2400,
+        },
+        lat: 53.2,
+        lng: 50,
+    },
+    {
+        name: 'Place 7',
+        address: 'Address 7',
+        distance: {
+            humanReadable: '2.1 km',
+            inMeters: 2100,
+        },
+        duration: {
+            humanReadable: '16 min',
+            inSeconds: 960,
+        },
+        lat: 53.2,
+        lng: 50.1,
+    },
+    {
+        name: 'Place 8',
+        address: 'Address 8',
+        distance: {
+            humanReadable: '0.5 km',
+            inMeters: 500,
+        },
+        duration: {
+            humanReadable: '5 min',
+            inSeconds: 300,
+        },
+        lat: 53.2,
+        lng: 50.2,
+    },
+    {
+        name: 'Place 9',
+        address: 'Address 9',
+        distance: {
+            humanReadable: '1.2 km',
+            inMeters: 1200,
+        },
+        duration: {
+            humanReadable: '8 min',
+            inSeconds: 480,
+        },
+        lat: 53.2,
+        lng: 50.3,
+    },
+    {
+        name: 'Place 10',
+        address: 'Address 10',
+        distance: {
+            humanReadable: '0.1 km',
+            inMeters: 100,
+        },
+        duration: {
+            humanReadable: '1 min',
+            inSeconds: 60,
+        },
+        lat: 53.2,
+        lng: 50.4,
+    },
+    {
+        name: 'Place 11',
+        address: 'Address 11',
+        distance: {
+            humanReadable: '1 km',
+            inMeters: 1000,
+        },
+        duration: {
+            humanReadable: '10 min',
+            inSeconds: 600,
+        },
+        lat: 53.2,
+        lng: 50.5,
+    },
+]
+
 
 class SearchPlacesPage extends Component {
     state = {
@@ -61,9 +218,9 @@ class SearchPlacesPage extends Component {
     }
 
     renderPlacesOnMap = places => {
-        const bounds = new google.maps.LatLngBounds()
-        places.forEach(p => {
-            let contentString = `
+            const bounds = new google.maps.LatLngBounds()
+            places.forEach(p => {
+                let contentString = `
                 Name: ${p.name}<br>
                 Address: ${p.address}
                 `
@@ -105,6 +262,7 @@ class SearchPlacesPage extends Component {
                 script.src = `https://maps.googleapis.com/maps/api/js?key=${
                     process.env.GOOGLE_KEY
                 }&libraries=places&callback=resolveGoogleMapsPromise`
+                // script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyD7kYHwzqUSGOKSoTq13TB8iMhgxpDJXj0&libraries=places&callback=resolveGoogleMapsPromise`
                 script.async = true
                 document.body.appendChild(script)
             })
--- src/main/resources/react-app/src/containers/SearchTicketsPage/index.js
+++ src/main/resources/react-app/src/containers/SearchTicketsPage/index.js
@@ -4,7 +4,7 @@ import SearchPlacesPageView from '../../components/SearchTicketsPageView'
 
 const d =
     '{"Dates":{"OutboundDates":[{"PartialDate":"2018-05-11","QuoteIds":[1],"Price":140.0,"QuoteDateTime":"2018-03-08T20:35:00"},{"PartialDate":"2018-05-16","QuoteIds":[2],"Price":85.0,"QuoteDateTime":"2018-03-06T05:44:00"},{"PartialDate":"2018-05-17","QuoteIds":[3,4,5],"Price":92.0,"QuoteDateTime":"2018-03-05T17:41:00"},{"PartialDate":"2018-05-18","QuoteIds":[6],"Price":130.0,"QuoteDateTime":"2018-03-06T06:18:00"},{"PartialDate":"2018-05-20","QuoteIds":[7],"Price":92.0,"QuoteDateTime":"2018-03-05T04:46:00"},{"PartialDate":"2018-05-21","QuoteIds":[8],"Price":112.0,"QuoteDateTime":"2018-03-01T04:51:00"},{"PartialDate":"2018-05-22","QuoteIds":[9],"Price":152.0,"QuoteDateTime":"2018-03-08T22:17:00"},{"PartialDate":"2018-05-23","QuoteIds":[10],"Price":103.0,"QuoteDateTime":"2018-03-06T02:16:00"},{"PartialDate":"2018-05-24","QuoteIds":[11],"Price":92.0,"QuoteDateTime":"2018-03-05T21:09:00"},{"PartialDate":"2018-05-25","QuoteIds":[12,13,14],"Price":115.0,"QuoteDateTime":"2018-03-04T17:45:00"},{"PartialDate":"2018-05-26","QuoteIds":[15,16,17],"Price":111.0,"QuoteDateTime":"2018-03-02T21:22:00"},{"PartialDate":"2018-05-27","QuoteIds":[18,19,20],"Price":110.0,"QuoteDateTime":"2018-03-06T21:04:00"},{"PartialDate":"2018-05-28","QuoteIds":[21,22,23,24],"Price":93.0,"QuoteDateTime":"2018-03-02T17:47:00"},{"PartialDate":"2018-05-30","QuoteIds":[25],"Price":93.0,"QuoteDateTime":"2018-03-02T03:23:00"},{"PartialDate":"2018-05-31","QuoteIds":[26,27],"Price":91.0,"QuoteDateTime":"2018-03-05T23:47:00"}],"InboundDates":[{"PartialDate":"2018-06-22","QuoteIds":[1],"Price":140.0,"QuoteDateTime":"2018-03-08T20:35:00"},{"PartialDate":"2018-06-07","QuoteIds":[2],"Price":85.0,"QuoteDateTime":"2018-03-06T05:44:00"},{"PartialDate":"2018-06-01","QuoteIds":[3],"Price":98.0,"QuoteDateTime":"2018-03-07T21:07:00"},{"PartialDate":"2018-06-02","QuoteIds":[4,9,11,12,18,21],"Price":92.0,"QuoteDateTime":"2018-03-05T11:37:00"},{"PartialDate":"2018-06-04","QuoteIds":[5,13,26],"Price":91.0,"QuoteDateTime":"2018-03-05T01:56:00"},{"PartialDate":"2018-06-03","QuoteIds":[6,19,22],"Price":130.0,"QuoteDateTime":"2018-03-06T06:18:00"},{"PartialDate":"2018-06-06","QuoteIds":[7],"Price":92.0,"QuoteDateTime":"2018-03-05T04:46:00"},{"PartialDate":"2018-06-11","QuoteIds":[8],"Price":112.0,"QuoteDateTime":"2018-03-01T04:51:00"},{"PartialDate":"2018-06-15","QuoteIds":[10],"Price":103.0,"QuoteDateTime":"2018-03-06T02:16:00"},{"PartialDate":"2018-06-17","QuoteIds":[14,15],"Price":124.0,"QuoteDateTime":"2018-03-02T21:22:00"},{"PartialDate":"2018-06-21","QuoteIds":[16],"Price":117.0,"QuoteDateTime":"2018-03-03T13:08:00"},{"PartialDate":"2018-06-30","QuoteIds":[17],"Price":111.0,"QuoteDateTime":"2018-03-04T23:54:00"},{"PartialDate":"2018-06-10","QuoteIds":[20],"Price":110.0,"QuoteDateTime":"2018-03-06T21:04:00"},{"PartialDate":"2018-06-12","QuoteIds":[23],"Price":1386.0,"QuoteDateTime":"2018-03-05T19:51:00"},{"PartialDate":"2018-06-16","QuoteIds":[24],"Price":93.0,"QuoteDateTime":"2018-03-02T17:47:00"},{"PartialDate":"2018-06-05","QuoteIds":[25,27],"Price":92.0,"QuoteDateTime":"2018-03-02T03:23:00"}]},"Quotes":[{"QuoteId":1,"MinPrice":140.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-11T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-22T00:00:00"},"QuoteDateTime":"2018-03-08T20:35:00"},{"QuoteId":2,"MinPrice":85.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-16T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-07T00:00:00"},"QuoteDateTime":"2018-03-06T05:44:00"},{"QuoteId":3,"MinPrice":98.0,"Direct":true,"OutboundLeg":{"CarrierIds":[1793],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-17T00:00:00"},"InboundLeg":{"CarrierIds":[1793],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-01T00:00:00"},"QuoteDateTime":"2018-03-07T21:07:00"},{"QuoteId":4,"MinPrice":92.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-17T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-02T00:00:00"},"QuoteDateTime":"2018-03-05T17:41:00"},{"QuoteId":5,"MinPrice":118.0,"Direct":true,"OutboundLeg":{"CarrierIds":[1793],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-17T00:00:00"},"InboundLeg":{"CarrierIds":[1793],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-04T00:00:00"},"QuoteDateTime":"2018-03-08T10:45:00"},{"QuoteId":6,"MinPrice":130.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-18T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-03T00:00:00"},"QuoteDateTime":"2018-03-06T06:18:00"},{"QuoteId":7,"MinPrice":92.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-20T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-06T00:00:00"},"QuoteDateTime":"2018-03-05T04:46:00"},{"QuoteId":8,"MinPrice":112.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-21T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-11T00:00:00"},"QuoteDateTime":"2018-03-01T04:51:00"},{"QuoteId":9,"MinPrice":152.0,"Direct":true,"OutboundLeg":{"CarrierIds":[1793],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-22T00:00:00"},"InboundLeg":{"CarrierIds":[1793],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-02T00:00:00"},"QuoteDateTime":"2018-03-08T22:17:00"},{"QuoteId":10,"MinPrice":103.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-23T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-15T00:00:00"},"QuoteDateTime":"2018-03-06T02:16:00"},{"QuoteId":11,"MinPrice":92.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-24T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-02T00:00:00"},"QuoteDateTime":"2018-03-05T21:09:00"},{"QuoteId":12,"MinPrice":115.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-25T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-02T00:00:00"},"QuoteDateTime":"2018-03-05T21:46:00"},{"QuoteId":13,"MinPrice":116.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-25T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-04T00:00:00"},"QuoteDateTime":"2018-03-05T01:56:00"},{"QuoteId":14,"MinPrice":133.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-25T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-17T00:00:00"},"QuoteDateTime":"2018-03-04T17:45:00"},{"QuoteId":15,"MinPrice":124.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-26T00:00:00"},"InboundLeg":{"CarrierIds":[1902],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-17T00:00:00"},"QuoteDateTime":"2018-03-02T21:22:00"},{"QuoteId":16,"MinPrice":117.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-26T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-21T00:00:00"},"QuoteDateTime":"2018-03-03T13:08:00"},{"QuoteId":17,"MinPrice":111.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-26T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-30T00:00:00"},"QuoteDateTime":"2018-03-04T23:54:00"},{"QuoteId":18,"MinPrice":119.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-27T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-02T00:00:00"},"QuoteDateTime":"2018-03-07T04:07:00"},{"QuoteId":19,"MinPrice":134.0,"Direct":true,"OutboundLeg":{"CarrierIds":[1793],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-27T00:00:00"},"InboundLeg":{"CarrierIds":[1793],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-03T00:00:00"},"QuoteDateTime":"2018-03-07T15:02:00"},{"QuoteId":20,"MinPrice":110.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-27T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-10T00:00:00"},"QuoteDateTime":"2018-03-06T21:04:00"},{"QuoteId":21,"MinPrice":171.0,"Direct":true,"OutboundLeg":{"CarrierIds":[1793],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-28T00:00:00"},"InboundLeg":{"CarrierIds":[1793],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-02T00:00:00"},"QuoteDateTime":"2018-03-05T11:37:00"},{"QuoteId":22,"MinPrice":135.0,"Direct":true,"OutboundLeg":{"CarrierIds":[1793],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-28T00:00:00"},"InboundLeg":{"CarrierIds":[1793],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-03T00:00:00"},"QuoteDateTime":"2018-03-09T01:17:00"},{"QuoteId":23,"MinPrice":1386.0,"Direct":false,"OutboundLeg":{"CarrierIds":[],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-28T00:00:00"},"InboundLeg":{"CarrierIds":[1169],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-12T00:00:00"},"QuoteDateTime":"2018-03-05T19:51:00"},{"QuoteId":24,"MinPrice":93.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-28T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-16T00:00:00"},"QuoteDateTime":"2018-03-02T17:47:00"},{"QuoteId":25,"MinPrice":93.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-30T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-05T00:00:00"},"QuoteDateTime":"2018-03-02T03:23:00"},{"QuoteId":26,"MinPrice":91.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-31T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-04T00:00:00"},"QuoteDateTime":"2018-03-07T21:37:00"},{"QuoteId":27,"MinPrice":92.0,"Direct":true,"OutboundLeg":{"CarrierIds":[851],"OriginId":81727,"DestinationId":65368,"DepartureDate":"2018-05-31T00:00:00"},"InboundLeg":{"CarrierIds":[851],"OriginId":65368,"DestinationId":81727,"DepartureDate":"2018-06-05T00:00:00"},"QuoteDateTime":"2018-03-05T23:47:00"}],"Places":[{"PlaceId":65368,"IataCode":"LAX","Name":"Los Angeles International","Type":"Station","SkyscannerCode":"LAX","CityName":"Los Angeles","CityId":"LAXA","CountryName":"United States"},{"PlaceId":81727,"IataCode":"SFO","Name":"San Francisco International","Type":"Station","SkyscannerCode":"SFO","CityName":"San Francisco","CityId":"SFOA","CountryName":"United States"}],"Carriers":[{"CarrierId":851,"Name":"Alaska Airlines"},{"CarrierId":1169,"Name":"Hawaiian Airlines"},{"CarrierId":1793,"Name":"United"},{"CarrierId":1902,"Name":"Southwest Airlines"}],"Currencies":[{"Code":"USD","Symbol":"$","ThousandsSeparator":",","DecimalSeparator":".","SymbolOnLeft":true,"SpaceBetweenAmountAndSymbol":false,"RoundingCoefficient":0,"DecimalDigits":2}]}'
-const data = JSON.parse(d)
+const dataTest = JSON.parse(d)
 
 class SearchTicketsPage extends Component {
     state = {
@@ -45,7 +45,7 @@ class SearchTicketsPage extends Component {
         })
             .then(response => response.json())
             .then(data => this.getTickets(data))
-            .catch(reason => this.setState({ ticketsNotFound: true }))
+            .catch(reason => this.getTickets(dataTest))
     }
 
     getTickets = data => {
@@ -119,8 +119,8 @@ class SearchTicketsPage extends Component {
         return fetch(url, {
             headers: {
                 'X-RapidAPI-Key':
-                'd8feb41877msha2eca9d57d913cap172a98jsn9481446e52eb'
-                    // 'Td24bfq18lmsh5KqGTIsh6amrNJRp1h4MngjsnIdn2a8CFcrl5',
+                // 'd8feb41877msha2eca9d57d913cap172a98jsn9481446e52eb'
+                    'Td24bfq18lmsh5KqGTIsh6amrNJRp1h4MngjsnIdn2a8CFcrl5',
             },
         })
             .then(response => response.json())
@@ -134,27 +134,27 @@ class SearchTicketsPage extends Component {
         })
     }
 
-    onSearch = () => {
-        this.getTickets(data)
-    }
-
     // onSearch = () => {
-    //     this.clearResults()
-    //     const departureDateFormat = this.formatDate(this.state.departureDate)
-    //     const returnDateFormat = this.formatDate(this.state.returnDate)
-    //
-    //     Promise.all([
-    //         this.getAirportId(this.state.departureCity),
-    //         this.getAirportId(this.state.arrivalCity),
-    //     ]).then(([departureAirport, arrivalAirport]) => {
-    //         this.loadData({
-    //             departureAirport,
-    //             arrivalAirport,
-    //             departureDateFormat,
-    //             returnDateFormat,
-    //         })
-    //     })
+    //     this.getTickets(data)
     // }
+
+    onSearch = () => {
+        this.clearResults()
+        const departureDateFormat = this.formatDate(this.state.departureDate)
+        const returnDateFormat = this.formatDate(this.state.returnDate)
+
+        Promise.all([
+            this.getAirportId(this.state.departureCity),
+            this.getAirportId(this.state.arrivalCity),
+        ]).then(([departureAirport, arrivalAirport]) => {
+            this.loadData({
+                departureAirport,
+                arrivalAirport,
+                departureDateFormat,
+                returnDateFormat,
+            })
+        })
+    }
 
     // onSearch2 = async () => {
     //     const departureDateFormat = this.formatDate(this.state.departureDate)
